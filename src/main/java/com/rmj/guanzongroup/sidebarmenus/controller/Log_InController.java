1ad63432e9da28aa176c40b06545a8358d8fa979
itsjohndavea <johndaveaquino35@gmail.com>
2025-02-26 11:37:30 +0800

    added border color effects when selecting textfields

--- src/main/java/com/rmj/guanzongroup/sidebarmenus/controller/Log_InController.java
+++ src/main/java/com/rmj/guanzongroup/sidebarmenus/controller/Log_InController.java
@@ -17,6 +17,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 
 /**
  * FXML Controller class
@@ -29,17 +31,18 @@ public class Log_InController implements Initializable, ScreenInterface {
     private GRider oApp;
 
     @FXML
-    private TextField txtField01;
+    private HBox hboxUserName, hboxPassword;
     @FXML
-    private Button btnSignIn;
+    private BorderPane borderPane;
+    @FXML
+    private TextField txtField01, txtField03;
+    @FXML
+    private Button btnSignIn, btnEyeIcon;
     @FXML
     Label lblCopyright;
-    @FXML
-    private TextField txtField03;
+
     @FXML
     private PasswordField txtField02;
-    @FXML
-    private Button btnEyeIcon;
 
     @Override
     public void setGRider(GRider foValue) {
@@ -50,40 +53,105 @@ public class Log_InController implements Initializable, ScreenInterface {
      */
     private DashboardController dashboardController;
 
+    public void setMainController(DashboardController controller) {
+        this.dashboardController = controller;
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         DashboardController mainController = LoginControllerHolder.getMainController();
         mainController.triggervbox();
 
         txtField03.textProperty().bindBidirectional(txtField02.textProperty());
-        String year = String.valueOf(Year.now().getValue());
-        lblCopyright.setText("© " + year + " Guanzon Group of Companies. All Rights Reserved.");
-        showText();
-    }
 
-    public void setMainController(DashboardController controller) {
-        this.dashboardController = controller;
-    }
+        showYearCopyRights();
+        setupPasswordToggle();
+        setupBorderHandling();
 
-    private void showText() {
-        FontAwesomeIconView eyeIcon = new FontAwesomeIconView(FontAwesomeIcon.EYE);
-        btnEyeIcon.setOnMouseClicked(event -> {
-            if (txtField02.isVisible()) {
-                txtField03.setText(txtField02.getText());
-                txtField02.setVisible(false);
-                txtField03.setVisible(true);
-                eyeIcon.setIcon(FontAwesomeIcon.EYE_SLASH);
-                eyeIcon.setStyle("-fx-fill: gray; -glyph-size: 20; ");
-                btnEyeIcon.setGraphic(eyeIcon);
-            } else {
-                txtField02.setText(txtField03.getText());
-                txtField03.setVisible(false);
-                txtField02.setVisible(true);
-                eyeIcon.setIcon(FontAwesomeIcon.EYE);
-                eyeIcon.setStyle("-fx-fill: gray; -glyph-size: 20; ");
-                btnEyeIcon.setGraphic(eyeIcon);
+        borderPane.setOnMouseClicked(event -> {
+            if (!(event.getTarget() instanceof TextField) && !(event.getTarget() instanceof FontAwesomeIconView)) {
+                removeAllBorders();
+                borderPane.requestFocus();
             }
         });
+    }
+
+    private void showYearCopyRights() {
+        lblCopyright.setText("© " + Year.now().getValue() + " Guanzon Group of Companies. All Rights Reserved.");
+    }
+
+    private void setupPasswordToggle() {
+        btnEyeIcon.setOnMouseClicked(event -> togglePasswordVisibility());
+
+        txtField03.focusedProperty().addListener((obs, oldVal, newVal) -> {
+            if (!newVal) {
+                hidePlainText();
+            }
+        });
+
+        txtField02.focusedProperty().addListener((obs, oldVal, newVal) -> {
+            if (!newVal && !txtField03.isFocused()) {
+                removeBorder(hboxPassword);
+            }
+        });
+    }
+
+    private void togglePasswordVisibility() {
+        boolean isPasswordVisible = txtField02.isVisible();
+        txtField03.setText(txtField02.getText());
+
+        txtField02.setVisible(!isPasswordVisible);
+        txtField03.setVisible(isPasswordVisible);
+
+        (isPasswordVisible ? txtField03 : txtField02).requestFocus();
+
+        updateEyeIcon(isPasswordVisible ? FontAwesomeIcon.EYE_SLASH : FontAwesomeIcon.EYE);
+        setBorder(hboxPassword, true);
+    }
+
+    private void hidePlainText() {
+        if (txtField03.isVisible()) {
+            txtField02.setText(txtField03.getText());
+            txtField03.setVisible(false);
+            txtField02.setVisible(true);
+            updateEyeIcon(FontAwesomeIcon.EYE);
+        }
+    }
+
+    private void updateEyeIcon(FontAwesomeIcon icon) {
+        FontAwesomeIconView eyeIcon = new FontAwesomeIconView(icon);
+        eyeIcon.setStyle("-fx-fill: gray; -glyph-size: 20;");
+        btnEyeIcon.setGraphic(eyeIcon);
+    }
+
+    private void setupBorderHandling() {
+        setBorderWeightTextField(txtField01, hboxUserName);
+        setBorderWeightTextField(txtField03, hboxPassword);
+        setBorderWeightTextField(txtField02, hboxPassword);
+    }
+
+    private void setBorderWeightTextField(TextField textField, HBox parentBox) {
+        textField.setOnMouseClicked(event -> setBorder(parentBox, true));
+
+        textField.focusedProperty().addListener((obs, oldVal, newVal) -> {
+            if (!newVal) {
+                removeBorder(parentBox);
+            }
+        });
+    }
+
+    private void setBorder(HBox parentBox, boolean isActive) {
+        parentBox.getStyleClass().removeAll("hbox-default", "hbox-border-width");
+        parentBox.getStyleClass().add(isActive ? "hbox-border-width" : "hbox-default");
+    }
+
+    private void removeBorder(HBox parentBox) {
+        setBorder(parentBox, false);
+    }
+
+    private void removeAllBorders() {
+        removeBorder(hboxUserName);
+        removeBorder(hboxPassword);
     }
 
     @FXML
