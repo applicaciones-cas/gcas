f57b46bf2a834ddfd984e7a6db4d9bd72ddb797e
Aldristh456034 <trinidadaldrich328@gmail.com>
2025-06-13 09:59:08 +0800

    06132025 - JFXUtil update

--- src/main/java/com/rmj/guanzongroup/sidebarmenus/utility/JFXUtil.java
+++ src/main/java/com/rmj/guanzongroup/sidebarmenus/utility/JFXUtil.java
@@ -25,8 +25,11 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.UnaryOperator;
+import javafx.animation.PauseTransition;
 import javafx.application.Platform;
+import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.ReadOnlyProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
@@ -80,6 +83,7 @@ import javafx.stage.Modality;
 import javafx.stage.Popup;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.util.Pair;
 import javafx.util.StringConverter;
 import org.apache.poi.ss.formula.functions.T;
 import org.json.simple.JSONObject;
@@ -607,6 +611,7 @@ public class JFXUtil {
     }
 
     public static boolean isTextFieldContainsStyleClass(String lsCssClassName, TextField... textFields) {
+        //used for removal
         for (TextField tf : textFields) {
             if (tf.getStyleClass().contains(lsCssClassName)) {
                 return true;
@@ -618,6 +623,15 @@ public class JFXUtil {
     public static void setDisabled(boolean disable, Node... nodes) {
         for (Node node : nodes) {
             node.setDisable(disable);
+            if (node instanceof TextField) {
+                if (disable) {
+                    while (node.getStyleClass().contains("DisabledTextField")) {
+                        node.getStyleClass().remove("DisabledTextField");
+                    }
+                } else {
+                    node.getStyleClass().add("DisabledTextField");
+                }
+            }
         }
     }
 
@@ -745,6 +759,12 @@ public class JFXUtil {
         }
     }
 
+    private static class scrollOffset {
+
+        double y;
+        int caretPos;
+    }
+
     public static void setVerticalScroll(TextArea textArea) {
         textArea.applyCss();
         textArea.layout();
@@ -757,22 +777,8 @@ public class JFXUtil {
                 + "-fx-text-fill: black;"
                 + "-fx-border-color: grey;"
         );
-        textArea.focusedProperty().addListener((obs, oldVal, isFocused) -> {
-            if (isFocused) {
-                textArea.setStyle(
-                        "-fx-background-color: orange, white;"
-                        + "-fx-text-fill: black;"
-                        + "-fx-border-color: orange;"
-                );
-            } else {
-                textArea.setStyle(
-                        "-fx-background-color: grey, white;"
-                        + "-fx-text-fill: black;"
-                        + "-fx-border-color: grey;"
-                );
-            }
-        });
 
+        // Access the internal ScrollPane
         ScrollPane scrollPane = (ScrollPane) textArea.lookup(".scroll-pane");
         if (scrollPane != null) {
             scrollPane.setStyle(
@@ -781,8 +787,65 @@ public class JFXUtil {
                     + "-fx-background-color: transparent;"
                     + "-fx-opacity: 1.0;"
             );
-        } else {
+
+            // Track vertical scroll position
+            final scrollOffset xyOffset = new scrollOffset();
+            final scrollOffset state = new scrollOffset();
+
+            scrollPane.vvalueProperty().addListener((obs, oldVal, newVal) -> {
+                if (newVal.intValue() == 0) {
+
+                } else {
+                    xyOffset.y = newVal.doubleValue();
+                    System.out.println(xyOffset.y);
+                }
+            });
+            textArea.caretPositionProperty().addListener((obs, oldVal, newVal) -> {
+                if (newVal.intValue() <= 0) {
+
+                } else {
+                    state.caretPos = newVal.intValue();
+                }
+            });
+            // Restore scroll position on focus lost
+            textArea.focusedProperty().addListener((obs, oldVal, isFocused) -> {
+                if (isFocused) {
+                    textArea.setStyle(
+                            "-fx-background-color: orange, white;"
+                            + "-fx-text-fill: black;"
+                            + "-fx-border-color: orange;"
+                    );
+
+                } else {
+                    textArea.setStyle(
+                            "-fx-background-color: grey, white;"
+                            + "-fx-text-fill: black;"
+                            + "-fx-border-color: grey;"
+                    );
+                    // Restore previous scroll position
+//                    Platform.runLater(() -> {
+//                        Platform.runLater(() -> {
+//                            scrollPane.setVvalue(xyOffset.y);
+//                            xyOffset.y = 0.0;
+//                        });
+//                    });
+//                    PauseTransition delay = new PauseTransition(Duration.millis(10));
+//                    delay.setOnFinished(e -> {
+//                        scrollPane.setVvalue(xyOffset.y);
+//                        xyOffset.y = 0.0;
+//                    });
+//                    delay.play();
+//                    Platform.runLater(() -> {
+//                        // Delay to override internal behavior
+//                        Platform.runLater(() -> {
+//                            textArea.positionCaret(state.caretPos);
+//                            state.caretPos = 0;
+//                        });
+//                    });
+                }
+            });
         }
+
         textArea.getStyleClass().add("custom-text-area");
     }
 
@@ -818,14 +881,20 @@ public class JFXUtil {
 
     public static String getFormattedClassTitle(Class<?> javaclass) {
         String className = javaclass.getSimpleName();
+
         if (className.endsWith("Controller")) {
             className = className.substring(0, className.length() - "Controller".length());
         }
+
+        // Handle specific company renaming
         className = className.replace("MonarchFood", "MF");
         className = className.replace("MonarchHospitality", "MH");
 
+        // Add space before capital letters, but keep acronyms like SOA, SI intact
         className = className.replace("_", " ");
+        className = className.replaceAll("(?<=[A-Z])(?=[A-Z][a-z])", " "); // Break after acronyms like SOA
         className = className.replaceAll("(?<=[a-z])(?=[A-Z])", " ");
+
         System.out.println(className.trim());
         return className.trim();
     }
@@ -909,17 +978,28 @@ public class JFXUtil {
         return (String) json.get("message");
     }
 
-    private static boolean isUpdating = false;
-    private static AtomicBoolean isAdjusting = new AtomicBoolean(false);
-    private static int newCaretPos = 0;
+    private static class CommaFormater {
+
+        boolean isUpdating;
+        AtomicBoolean isAdjusting;
+        int newCaretPos;
+    }
+//    private static boolean isUpdating = false;
+//   private static  AtomicBoolean isAdjusting = new AtomicBoolean(false);
+//    private static int newCaretPos = 0;
 
     public static void setCommaFormatter(TextField... textFields) {
+
         DecimalFormat finalFormat = (DecimalFormat) NumberFormat.getNumberInstance(Locale.US);
         finalFormat.setGroupingUsed(true);
         finalFormat.setMinimumFractionDigits(2);
         finalFormat.setMaximumFractionDigits(2);
 
         for (TextField textField : textFields) {
+            final CommaFormater data = new CommaFormater();
+            data.isUpdating = false;
+            data.isAdjusting = new AtomicBoolean(false);
+            data.newCaretPos = 0;
             // Disables other character
             UnaryOperator<TextFormatter.Change> filter = change -> {
                 String newText = change.getControlNewText();
@@ -937,17 +1017,17 @@ public class JFXUtil {
             textField.setTextFormatter(new TextFormatter<>(filter));
             // Real-time formatting
             textField.textProperty().addListener((obs, oldValue, newValue) -> {
-                if (isAdjusting.get() == true) {
+                if (data.isAdjusting.get() == true) {
                     return;
                 }
                 try {
-                    if (isUpdating) {
+                    if (data.isUpdating) {
                         return;
                     }
-                    isUpdating = true;
+                    data.isUpdating = true;
                     String clean = newValue.replaceAll(",", "");
                     if (clean.isEmpty() || clean.equals(".") || clean.matches("0*\\.0*")) {
-                        isUpdating = false;
+                        data.isUpdating = false;
                         return;
                     }
                     try {
@@ -962,22 +1042,22 @@ public class JFXUtil {
                         String formattedInteger = NumberFormat.getIntegerInstance(Locale.US).format(integerVal);
                         String formatted = formattedInteger + decimalPart;
                         Platform.runLater(() -> {
-                            isAdjusting.set(true);
+                            data.isAdjusting.set(true);
                             int originalCaretPos = textField.getCaretPosition();
                             textField.setText(formatted);
                             int offset = formatted.length() - newValue.length();
-                            newCaretPos = originalCaretPos + offset;
-                            newCaretPos = Math.max(0, Math.min(formatted.length(), newCaretPos));
-                            isAdjusting.set(false);
+                            data.newCaretPos = originalCaretPos + offset;
+                            data.newCaretPos = Math.max(0, Math.min(formatted.length(), data.newCaretPos));
+                            data.isAdjusting.set(false);
                         });
                         Platform.runLater(() -> {
-                            textField.positionCaret(newCaretPos);
+                            textField.positionCaret(data.newCaretPos);
                         });
                     } catch (Exception e) {
                     }
-                    isUpdating = false;
+                    data.isUpdating = false;
                 } catch (Exception e) {
-                    isUpdating = false;
+                    data.isUpdating = false;
                 }
 
             });
@@ -1228,5 +1308,22 @@ public class JFXUtil {
         }
         String result = numberStr.replace(",", "");
         return result.isEmpty() ? "0" : result;
+    }
+
+    public static void showRetainedHighlight(boolean isRetained, TableView<?> tblView, String color, List<Pair<String, String>> plOrderNoPartial, List<Pair<String, String>> plOrderNoFinal, Map<String, List<String>> highlightedRows) {
+        if (isRetained) {
+            for (Pair<String, String> pair : plOrderNoPartial) {
+                if (!"0".equals(pair.getValue())) {
+                    plOrderNoFinal.add(new Pair<>(pair.getKey(), pair.getValue()));
+                }
+            }
+        }
+        disableAllHighlightByColor(tblView, color, highlightedRows);
+        plOrderNoPartial.clear();
+        for (Pair<String, String> pair : plOrderNoFinal) {
+            if (!"0".equals(pair.getValue())) {
+                highlightByKey(tblView, pair.getKey(), color, highlightedRows);
+            }
+        }
     }
 }
