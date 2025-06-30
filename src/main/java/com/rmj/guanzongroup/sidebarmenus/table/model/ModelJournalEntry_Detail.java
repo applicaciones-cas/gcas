9f81355551f6a5230feac60b508cb16e0fc5874b
r-sie <32829606+Arsiela@users.noreply.github.com>
2025-06-30 16:05:34 +0800

    Update ModelJournalEntry_Detail.java

--- src/main/java/com/rmj/guanzongroup/sidebarmenus/table/model/ModelJournalEntry_Detail.java
+++ src/main/java/com/rmj/guanzongroup/sidebarmenus/table/model/ModelJournalEntry_Detail.java
@@ -5,6 +5,8 @@
 package com.rmj.guanzongroup.sidebarmenus.table.model;
 
 import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 /**
  *
@@ -22,6 +24,19 @@ public class ModelJournalEntry_Detail {
     public SimpleStringProperty index08;
     public SimpleStringProperty index09;
     public SimpleStringProperty index10;
+
+    public ModelJournalEntry_Detail(String index01,
+            String index02,
+            String index03,
+            String index04,
+            String index05) {
+
+        this.index01 = new SimpleStringProperty(index01);
+        this.index02 = new SimpleStringProperty(index02);
+        this.index03 = new SimpleStringProperty(index03);
+        this.index04 = new SimpleStringProperty(index04);
+        this.index05 = new SimpleStringProperty(index05);
+    }
 
     public ModelJournalEntry_Detail(String index01,
             String index02,
@@ -53,7 +68,8 @@ public class ModelJournalEntry_Detail {
             String index04,
             String index05,
             String index06,
-            String index07
+            String index07,
+            String index08
     ) {
 
         this.index01 = new SimpleStringProperty(index01);
@@ -63,16 +79,17 @@ public class ModelJournalEntry_Detail {
         this.index05 = new SimpleStringProperty(index05);
         this.index06 = new SimpleStringProperty(index06);
         this.index07 = new SimpleStringProperty(index07);
+        this.index08 = new SimpleStringProperty(index08);
     }
 
-    public ModelJournalEntry_Detail(String index01,
+    public ModelJournalEntry_Detail(
+            String index01,
             String index02,
             String index03,
             String index04,
             String index05,
-            String index06,
-            String index07,
-            String index08) {
+            String index06
+    ) {
 
         this.index01 = new SimpleStringProperty(index01);
         this.index02 = new SimpleStringProperty(index02);
@@ -80,29 +97,6 @@ public class ModelJournalEntry_Detail {
         this.index04 = new SimpleStringProperty(index04);
         this.index05 = new SimpleStringProperty(index05);
         this.index06 = new SimpleStringProperty(index06);
-        this.index07 = new SimpleStringProperty(index07);
-        this.index08 = new SimpleStringProperty(index08);
-    }
-
-    public ModelJournalEntry_Detail(String index01,
-            String index02,
-            String index03,
-            String index04,
-            String index05,
-            String index06,
-            String index07,
-            String index08,
-            String index09) {
-
-        this.index01 = new SimpleStringProperty(index01);
-        this.index02 = new SimpleStringProperty(index02);
-        this.index03 = new SimpleStringProperty(index03);
-        this.index04 = new SimpleStringProperty(index04);
-        this.index05 = new SimpleStringProperty(index05);
-        this.index06 = new SimpleStringProperty(index06);
-        this.index07 = new SimpleStringProperty(index07);
-        this.index08 = new SimpleStringProperty(index08);
-        this.index09 = new SimpleStringProperty(index09);
     }
 
     public String getIndex01() {
@@ -176,13 +170,4 @@ public class ModelJournalEntry_Detail {
     public void setIndex09(String index09) {
         this.index09.set(index09);
     }
-
-    public String getIndex10() {
-        return index10.get();
-    }
-
-    public void setIndex10(String index10) {
-        this.index10.set(index10);
-    }
-
 }
