452ab4ae08e97390b30a59959639193431e29c04
Zendrex Adversalo <adversalozen8@gmail.com>
2025-07-07 21:18:33 +0800

    fixed branch code for entry

--- src/main/java/com/rmj/guanzongroup/sidebarmenus/controller/InvRequest_EntryMonarchGeneralController.java
+++ src/main/java/com/rmj/guanzongroup/sidebarmenus/controller/InvRequest_EntryMonarchGeneralController.java
@@ -211,7 +211,7 @@ public class InvRequest_EntryMonarchGeneralController implements Initializable, 
                     if ("success".equals((String) loJSON.get("result"))) {
                         invRequestController.StockRequest().Master().setIndustryId(psIndustryID);
                         invRequestController.StockRequest().Master().setCompanyID(psCompanyID);
-                        invRequestController.StockRequest().Master().setBranchCode(psBranchCode); 
+                        invRequestController.StockRequest().Master().setBranchCode(poApp.getBranchCode()); 
                         invRequestController.StockRequest().Master().setCategoryId(psCategoryID); 
                         System.out.println("Category asdasd+ "+ invRequestController.StockRequest().Master().getCategoryId());
                                 
@@ -523,7 +523,7 @@ public class InvRequest_EntryMonarchGeneralController implements Initializable, 
                     lsStatus = "VOID";
                     break;
             }
-              poJSON =invRequestController.StockRequest().SearchBranch(lsStatus, true);   
+             
               
                
             lblTransactionStatus.setText(lsStatus); //step 15-16
