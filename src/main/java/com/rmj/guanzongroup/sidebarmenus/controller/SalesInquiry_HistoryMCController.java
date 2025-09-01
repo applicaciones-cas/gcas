158a7fcea4bc93dc7d64bd9c8f546c9dc9cc2c01
Aldristh456034 <trinidadaldrich328@gmail.com>
2025-09-01 10:53:53 +0800

    09012025 - Sales inquiry controller
    - updated link

--- src/main/java/com/rmj/guanzongroup/sidebarmenus/controller/SalesInquiry_HistoryMCController.java
+++ src/main/java/com/rmj/guanzongroup/sidebarmenus/controller/SalesInquiry_HistoryMCController.java
@@ -331,7 +331,7 @@ public class SalesInquiry_HistoryMCController implements Initializable, ScreenIn
             tfModel.setText(poSalesInquiryController.SalesInquiry().Detail(pnDetail).Model().getDescription());
             tfModelVariant.setText(poSalesInquiryController.SalesInquiry().Detail(pnDetail).ModelVariant().getDescription());
             tfColor.setText(poSalesInquiryController.SalesInquiry().Detail(pnDetail).Color().getDescription());
-            tfSellingPrice.setText(CustomCommonUtil.setIntegerValueToDecimalFormat(poSalesInquiryController.SalesInquiry().Detail(pnDetail).Inventory().getSellingPrice(), true));
+            tfSellingPrice.setText(CustomCommonUtil.setIntegerValueToDecimalFormat(poSalesInquiryController.SalesInquiry().Detail(pnDetail).getSellPrice(), true));
             JFXUtil.updateCaretPositions(apDetail);
         } catch (SQLException | GuanzonException ex) {
             Logger.getLogger(getClass().getName()).log(Level.SEVERE, MiscUtil.getException(ex), ex);
