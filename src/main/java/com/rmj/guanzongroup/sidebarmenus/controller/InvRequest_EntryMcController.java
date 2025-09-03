80392abe4d3499db9c99237db63a3491ba30b134
Zendrex Adversalo <adversalozen8@gmail.com>
2025-09-03 10:24:26 +0800

    .

--- src/main/java/com/rmj/guanzongroup/sidebarmenus/controller/InvRequest_EntryMcController.java
+++ src/main/java/com/rmj/guanzongroup/sidebarmenus/controller/InvRequest_EntryMcController.java
@@ -1052,7 +1052,7 @@ public class InvRequest_EntryMcController implements Initializable, ScreenInterf
                                         invRequestController.StockRequest().Master().setCompanyID(psCompanyID);
                                         invRequestController.StockRequest().Master().setCategoryId(psCategoryID);
                                         invRequestController.StockRequest().setTransactionStatus("102");
-                                        poJSON = invRequestController.StockRequest().searchTransaction();
+                                        poJSON = invRequestController.StockRequest().searchTransaction(true);
                                         if (!"error".equals((String) poJSON.get("result"))) {
                                             pnTblInvDetailRow = -1;
                                             loadMaster();
