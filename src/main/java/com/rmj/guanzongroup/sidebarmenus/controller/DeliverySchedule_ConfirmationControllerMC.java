6aedc6159d1d923b294ffbb8816f3afc06edfa0f
MayNevVal <valenciamaynardn@gmail.com>
2025-09-25 17:25:30 +0800

    check transfer testphase

--- src/main/java/com/rmj/guanzongroup/sidebarmenus/controller/DeliverySchedule_ConfirmationControllerMC.java
+++ src/main/java/com/rmj/guanzongroup/sidebarmenus/controller/DeliverySchedule_ConfirmationControllerMC.java
@@ -394,7 +394,7 @@ public class DeliverySchedule_ConfirmationControllerMC implements Initializable,
                             break;
                         }
                         reloadTableDetail();
-                        clearAllInputs();
+//                        clearAllInputs();
                         pnEditMode = poAppController.getEditMode();
                         break;
                     }
@@ -418,7 +418,7 @@ public class DeliverySchedule_ConfirmationControllerMC implements Initializable,
 
                         }
                         reloadTableDetail();
-                        clearAllInputs();
+//                        clearAllInputs();
                         pnEditMode = poAppController.getEditMode();
                         break;
                     }
