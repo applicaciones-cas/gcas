1af99e7979dd0ec1afddac3505c9b79e3422f8fd
Aldristh456034 <trinidadaldrich328@gmail.com>
2025-10-09 16:10:46 +0800

    10092025 - si posting
    - added show current image

--- src/main/java/com/rmj/guanzongroup/sidebarmenus/controller/SIPosting_CarController.java
+++ src/main/java/com/rmj/guanzongroup/sidebarmenus/controller/SIPosting_CarController.java
@@ -321,6 +321,7 @@ public class SIPosting_CarController implements Initializable, ScreenInterface {
 
         }
         AttachmentDialogController controller = new AttachmentDialogController();
+        controller.setOpenedImage(pnAttachment);
         controller.addData(data);
         try {
             stageAttachment.showDialog((Stage) btnSave.getScene().getWindow(), getClass().getResource("/com/rmj/guanzongroup/sidebarmenus/views/AttachmentDialog.fxml"), controller, "Attachment Dialog", false, false, true);
